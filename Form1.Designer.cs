fe617aaebcc4086d8d1597dc78f400e25c467bf5
yael1009 <emilianozapataypanchovilla@gmail.com>
2026-09-07 15:38:54 -0600

    07/09/2026

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,69 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.tmrReloj = new System.Windows.Forms.Timer(this.components);
+            this.lblReloj = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.lblEjecucion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tmrReloj
+            // 
+            this.tmrReloj.Enabled = true;
+            this.tmrReloj.Interval = 1000;
+            this.tmrReloj.Tick += new System.EventHandler(this.tmrReloj_Tick);
+            // 
+            // lblReloj
+            // 
+            this.lblReloj.AutoSize = true;
+            this.lblReloj.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReloj.Location = new System.Drawing.Point(26, 27);
+            this.lblReloj.Name = "lblReloj";
+            this.lblReloj.Size = new System.Drawing.Size(63, 69);
+            this.lblReloj.TabIndex = 0;
+            this.lblReloj.Text = "0";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFecha.Location = new System.Drawing.Point(26, 124);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(63, 69);
+            this.lblFecha.TabIndex = 1;
+            this.lblFecha.Text = "0";
+            // 
+            // lblEjecucion
+            // 
+            this.lblEjecucion.AutoSize = true;
+            this.lblEjecucion.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEjecucion.Location = new System.Drawing.Point(26, 214);
+            this.lblEjecucion.Name = "lblEjecucion";
+            this.lblEjecucion.Size = new System.Drawing.Size(581, 69);
+            this.lblEjecucion.TabIndex = 2;
+            this.lblEjecucion.Text = "Tiempo en ejecucion";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(640, 331);
+            this.Controls.Add(this.lblEjecucion);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.lblReloj);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer tmrReloj;
+        private System.Windows.Forms.Label lblReloj;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Label lblEjecucion;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,28 @@ namespace MyApp01
 {
     public partial class Form1 : Form
     {
+        int contador;
+        int minutos;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void tmrReloj_Tick(object sender, EventArgs e)
+        {
+            //lblReloj.Text = DateTime.Now.ToShortTimeString();
+            //lblReloj.Text = DateTime.Now.ToLongTimeString();
+            int contador++;
+            DateTime tiempo  = DateTime.Now;
+            lblReloj.Text = tiempo.ToString("HH:mm:ss");
+            lblFecha.Text = tiempo.ToString("MM:dd:yyyy");
+            if (contador == 60) {
+                minutos++;
+                contador = 0;
+            }
+            lblEjecucion.Text = "Tiempo en ejecucion" + minutos.ToString()  + "minutos";
+
         }
     }
 }
